c7e7703a7f57df966f3f68203b9bd7e0ed2b41fe
Neil Wehneman <neil@wehneman.com>
2023-07-20 12:49:30 -0400

    initial commit

---
+++ StaticVsInstanceMethods/StaticVsInstanceMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StaticVsInstanceMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // This is a static method. It doesn't depend upon any given instantiated object,
+        // and is called based on the class name.
+        Quadrilateral.PrintNumberOfSidesAQuadrilateralHas();
+
+        Quadrilateral myParticularQuadrilateral = new Quadrilateral();
+
+        myParticularQuadrilateral.Side1Length = 3;
+        myParticularQuadrilateral.Side2Length = 4;
+        myParticularQuadrilateral.Side3Length = 5;
+
+        // This is an instance method. It depends upon a particular instantiated object,
+        // and is called based on the instantiated object's name.
+        myParticularQuadrilateral.PrintNumberOfSidesWithValue();
+
+        Console.ReadKey();
+    }
+}
---
+++ StaticVsInstanceMethods/StaticVsInstanceMethods/Quadrilateral.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace StaticVsInstanceMethods
+{
+	public class Quadrilateral
+	{
+		public int Side1Length { get; set; }
+		public int Side2Length { get; set; }
+		public int Side3Length { get; set; }
+		public int Side4Length { get; set; }
+
+		public static void PrintNumberOfSidesAQuadrilateralHas()
+		{
+			Console.WriteLine("A quadrilateral has 4 sides.");
+		}
+
+		public void PrintNumberOfSidesWithValue()
+		{
+			List<int> sidesWithAssignedValues = new List<int>();
+
+			if (Side1Length > 0)
+			{
+				sidesWithAssignedValues.Add(Side1Length);
+			}
+
+            if (Side2Length > 0)
+            {
+                sidesWithAssignedValues.Add(Side2Length);
+            }
+
+            if (Side3Length > 0)
+            {
+                sidesWithAssignedValues.Add(Side3Length);
+            }
+
+            if (Side4Length > 0)
+            {
+                sidesWithAssignedValues.Add(Side4Length);
+            }
+
+            Console.WriteLine("This quadrilateral has " + sidesWithAssignedValues.Count + " sides with assigned values");
+		}
+	}
+}
